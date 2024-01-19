e737e864940f1fcdbe3ef977aab763079b5abe5a
anon <anon>
2024-01-18 19:40:55 -0700

    assembly printing strings

--- bootsect.asm
+++ bootsect.asm
@@ -1,9 +1,23 @@
 [bits 16]
 [org 0x7c00]
 
-mov al, 'h'
 mov ah, 0x0e
-int 0x10
+mov bx, greeting
+
+printString:
+    mov al, [bx]
+    cmp al, 0
+    je end
+    int 0x10
+    inc bx
+    jmp printString
+end:
+
+jmp $
+
+greeting:
+    db "Hello, I am an operating system written in Assembly.",0
+
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
